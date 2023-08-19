a97bda95e488d9e161e0b1b1b52048f9f20f21cb
HappyPtis09 <bokunozaq@gmail.com>
2023-08-19 11:38:00 +0000

    puts

---
+++ _put.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ *
+ * @str: the string to print
+ *
+ * Return: Nothing
+ */
+int _puts(char *string)
+{
+	char *c = string;
+
+	while (*string)
+	{
+		_putchar(*string++);
+	}
+	return (string - c);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: 1; if error, -1
+ */
+int _putchar(int c)
+{
+	static int j;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || j >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, j);
+		j = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[j++] = c;
+	return (1);
+}
